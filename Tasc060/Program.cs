9c91a510ee1948151f43cdededa6563262b34061
«Evgenia <evgenia.kopitext@gmail.com>
2022-07-23 15:49:44 +1100

    Tasc060

---
+++ Tasc060/Program.cs
@@ -0,0 +1,75 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+// Что-то пошло не так, а что именно понять не могу
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцы массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите глубину массива: ");
+int depth = Convert.ToInt32(Console.ReadLine());
+
+int[,,] newArray = new int[rows, columns, depth];
+GetNewArray(newArray);
+// int[,] finishArray = new int[rows, columns];
+// GetArray(newArray);
+PrintArray(newArray);
+
+
+void GetNewArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write ("");
+            for (int k = 0; k < inputArray.GetLength(2); k++)
+            {
+                Console.WriteLine($"{inputArray[i, j, k]} ({i} {j} {k}) ");
+            }
+        }
+    }
+}
+
+
+
